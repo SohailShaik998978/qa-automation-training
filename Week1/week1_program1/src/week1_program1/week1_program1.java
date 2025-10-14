ce461a2b82b05f155ee43c85c22b8ae743bb81e7
SohailShaik998978 <125714332+SohailShaik998978@users.noreply.github.com>
2025-10-13 23:54:06 -0400

    Add files via upload

---
+++ Week1/week1_program1/src/week1_program1/week1_program1.java
@@ -0,0 +1,11 @@
+package week1_program1;
+
+public class week1_program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
